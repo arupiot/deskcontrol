0c3d689ce5a98c72d1839c7912a353e119a7949a
Ben Hussey <ben@blip2.net>
2017-09-27 15:12:26 +0100

    Fixed RFID and Netowrk modules. Added reboot and pull commands

--- deskcontrol/modules/network.py
+++ deskcontrol/modules/network.py
@@ -1,25 +1,111 @@
 from navigation import StateModule
 import netifaces as ni
+import subprocess
 
-def get_ip_address(iface):
-    ni.ifaddresses(iface)
-    return ni.ifaddresses(iface)[ni.AF_INET][0]['addr']
+
+def get_git_revision_short_hash():
+    try:
+        return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
+    except Exception as e:
+        print("Error Getting Get Revision:")
+        print(e)
+        return None
+
+
+def get_eth_address(iface="eth0"):
+    try:
+        ni.ifaddresses(iface)
+        return ni.ifaddresses(iface)[ni.AF_INET][0]['addr']
+    except Exception as e:
+        print("Error Getting IP Address:")
+        print(e)
+        return None
+
+
+def get_wlan_address(iface="wlan0"):
+    try:
+        ni.ifaddresses(iface)
+        return ni.ifaddresses(iface)[ni.AF_INET][0]['addr']
+    except Exception as e:
+        print("Error Getting IP Address:")
+        print(e)
+        return None
+
+
+def git_update():
+    subprocess.check_output(["git", "pull"])
+    subprocess.check_output(["sudo", "service", "deskcontrol", "restart"])
+
+
+def reboot_pi():
+    subprocess.check_output(["sudo", "reboot"])
 
 
 class NetworkModule(StateModule):
     menu_title = "Network"
-    ip = None
+    current = 0
+    triggered = False
+    menu = {
+        0: {
+            "title": "Wireless IP",
+            "get_value": get_wlan_address,
+        },
+        1: {
+            "title": "Ethernet IP",
+            "get_value": get_eth_address,
+        },
+        2: {
+            "title": "Code Version",
+            "get_value": get_git_revision_short_hash,
+            "trigger": git_update,
+        },
+        3: {
+            "title": "Reboot?",
+            "value": "",
+            "trigger": reboot_pi,
+        },
+    }
 
     def draw(self, clear=True):
-        try:
-            self.ip = get_ip_address("wlan0")
-        except Exception:
-            self.ip = None
+        key = self.menu.keys()[self.current]
+        menu = self.menu[key]
+        if "get_value" in menu:
+            value = menu["get_value"]()
+        else:
+            value = menu["value"]
         if clear:
             self.controller.screen.device.clear_display()
+        print(value)
         self.controller.screen.draw(
             "values",
-            {"title": "Wireless IP", "value": str(self.ip), })
+            {"title": menu["title"], "value": str(value).rstrip(), })
+
+    def trigger(self):
+        key = self.menu.keys()[self.current]
+        menu = self.menu[key]
+        if "trigger" in menu:
+            self.triggered = True
+            self.controller.screen.draw(
+                "values",
+                {"title": menu["title"], "value": "Please wait...", })
+            menu["trigger"]()
 
     def navigate(self, direction):
-        self.controller.prev_module()
+        if self.triggered:
+            return
+        if direction == "back":
+            self.controller.prev_module()
+        if direction == "forward":
+            self.trigger()
+            self.draw()
+        if direction in ["down", "up"]:
+            if direction == "down":
+                self.current = self.current + 1
+            else:
+                self.current = self.current - 1
+            if self.current >= len(self.menu):
+                self.current = 0
+            elif self.current < 0:
+                self.current = len(self.menu) - 1
+            # print("Output: " + str(list(self.outputs)[self.current]))
+            self.draw()
--- deskcontrol/modules/rfid.py
+++ deskcontrol/modules/rfid.py
@@ -7,11 +7,18 @@ class RFIDModule(StateModule):
     previous = None
     tick_count = 0
     tag_type = 0
+    auth = "Nobody"
+
+    users = {
+        "411713624212177128": "Ben",
+        "45014924212177128": "Mike",
+        "41166424212177128": "Francesco",
+    }
 
     def draw(self, clear=True):
         self.controller.screen.draw(
             "values",
-            {"title": "Hello,", "value": "     User!", })
+            {"title": "Hello,", "value": self.auth, })
 
     def read_card(self, state, idle, nr):
         if idle:
@@ -20,12 +27,13 @@ class RFIDModule(StateModule):
 
         if state == nr.STATE_REQUEST_TAG_ID_READY:
             ret = nr.get_tag_id()
-            print("Found tag of type " + str(ret.tag_type) + " with ID [" +
-                  " ".join(map(str, map(hex, ret.tid[:ret.tid_length]))) + "]")
-            if (not self.previous and self.controller.current_module and
-                    self.controller.current_module.id != "RFIDModule"):
-                self.previous = self.controller.current_module.id
-                self.controller.change_module("RFIDModule")
+            card = str("".join(map(str, ret.tid[:ret.tid_length])))
+            print(card)
+            if card in self.users:
+                self.auth = self.users[card]
+            else:
+                self.auth = "Unknown"
+            self.controller.change_module("RFIDModule")
 
     def try_bricklet(self, uid, device_identifier, position):
         if device_identifier == 246:
@@ -44,15 +52,9 @@ class RFIDModule(StateModule):
             # print("Created Card Reader")
 
     def navigate(self, direction):
-        if self.previous:
-            self.controller.change_module(self.previous)
-        else:
-            self.controller.change_module("MenuModule")
+        self.controller.change_module("MenuModule")
 
     def tick(self):
         self.tick_count += 1
         if self.tick_count > 5:
-            if self.previous:
-                self.controller.change_module(self.previous)
-            else:
-                self.controller.change_module("MenuModule")
+            self.controller.change_module("MenuModule")
