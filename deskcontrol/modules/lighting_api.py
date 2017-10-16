74fee1871e249879bd1cffc31acc9751d175b370
Ben Hussey <ben@blip2.net>
2017-10-16 23:01:42 +0100

    Added lighting via requests (for DCW)

---
+++ deskcontrol/modules/lighting_api.py
@@ -0,0 +1,106 @@
+from navigation import StateModule
+import requests
+
+
+class LightingModule(StateModule):
+    menu_title = "Lighting"
+
+    outputs = {}
+    edit = False
+    device = None
+    current = 0
+    intens = 100
+    color = 0
+    colors = [
+        {"name": "Warm", "color": "FF9922"},
+        {"name": "Cool", "color": "FFFF33"},
+        {"name": "Red", "color": "FF0000"},
+        {"name": "Blue", "color": "0000FF"},
+        {"name": "Magenta", "color": "FF00FF"},
+        {"name": "Green", "color": "00FF00"},
+    ]
+
+    def __init__(self, controller):
+        super(LightingModule, self).__init__(controller)
+        self.set_light()
+
+    def draw(self, clear=True):
+        if clear:
+            self.controller.screen.device.clear_display()
+        if not len(self.outputs):
+            self.controller.screen.draw("values", {})
+            return
+        outputs = self.outputs[self.outputs.keys()[self.current]]
+        if self.edit:
+            name = outputs["name"] + "*"
+        else:
+            name = outputs["name"]
+        if outputs["type"] == "dimmer":
+            self.controller.screen.draw(
+                "values",
+                {"title": name,
+                 "value": str(self.intens) + " %", })
+        if outputs["type"] == "select":
+            self.controller.screen.draw(
+                "values",
+                {"title": name,
+                 "value": str(self.colors[self.color]["name"]), })
+
+    def change_light(self, direction):
+        if self.outputs[self.outputs.keys()[self.current]]["type"] == "dimmer":
+            if direction == "up" and self.intens < 100:
+                self.intens += 10
+            elif direction == "down" and self.intens > 0:
+                self.intens -= 10
+        else:
+            if direction == "up":
+                if self.color >= len(self.colors) - 1:
+                    self.color = 0
+                else:
+                    self.color += 1
+            elif direction == "down":
+                if self.color <= 0:
+                    self.color = len(self.colors) - 1
+                else:
+                    self.color -= 1
+        # print "change " + direction + str(self.color)
+
+    def set_light(self):
+        if self.device:
+            color = self.colors[self.color]["color"]
+            self.intens
+            requests.get(
+                'http://192.168.2.101:8000/color/5/%s/' % color,
+                timeout=0.5)
+            requests.get(
+                'http://192.168.2.101:8000/color/5/%s/' % color,
+                timeout=0.5)
+
+    def navigate(self, direction):
+        if not self.edit:
+            if direction == "forward":
+                self.edit = True
+                self.draw()
+            if direction == "back":
+                self.controller.prev_module()
+            if direction in ["down", "up"]:
+                if direction == "down":
+                    self.current = self.current + 1
+                else:
+                    self.current = self.current - 1
+                if self.current >= len(self.outputs):
+                    self.current = 0
+                elif self.current < 0:
+                    self.current = len(self.outputs) - 1
+                # print "Output: " + str(list(self.outputs)[self.current])
+                self.draw()
+        else:
+            if direction == "back":
+                self.edit = False
+                self.draw()
+            if direction in ["down", "up"]:
+                self.change_light(direction)
+                self.draw()
+
+    def tick(self):
+        self.draw(clear=False)
